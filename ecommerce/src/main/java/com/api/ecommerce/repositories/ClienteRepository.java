0cc15fef1a9ba4919ec4a2e152717fc1aea8a7df
sabrinapereiry <sabrinapereira2125@gmail.com>
2023-10-19 16:20:46 -0300

    branch nova

---
+++ ecommerce/src/main/java/com/api/ecommerce/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.api.ecommerce.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.api.ecommerce.entities.Cliente;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long>{
+
+}
