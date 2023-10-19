6dad7148f6cb0666c8093772cc05e7382616bdbb
lucascaiafa00 <lucascaiafa00@gmail.com>
2023-10-19 16:15:03 -0300

    Atualização service, controller e repository do produto

--- ecommerce/src/main/java/com/api/ecommerce/controllers/ProdutoController.java
+++ ecommerce/src/main/java/com/api/ecommerce/controllers/ProdutoController.java
@@ -1,5 +1,60 @@
 package com.api.ecommerce.controllers;
 
-public class ProdutoController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.ecommerce.entities.Produto;
+import com.api.ecommerce.services.ProdutoService;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+	
+	@Autowired
+	ProdutoService produtoService;
+	
+	@PostMapping
+	public ResponseEntity<Produto> cadastrarProduto(@RequestBody Produto produto){
+		return new ResponseEntity<>(produtoService.cadastrarProduto(produto), HttpStatus.CREATED);
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<Produto>> listarProdutos(){
+		return new ResponseEntity<>(produtoService.listarProdutos(), HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Produto> buscarProdutoPorId(@PathVariable Long id){
+		Produto produto = produtoService.buscarProdutoPorId(id);
+		
+		if(produto == null) {
+			return new ResponseEntity<>(produto, HttpStatus.NOT_FOUND);
+		} else {
+			return new ResponseEntity<>(produto, HttpStatus.OK);
+		}
+	}
+	
+	@PutMapping
+	public ResponseEntity<Produto> atualizar(@RequestBody Produto produto) {
+		return new ResponseEntity<>(produtoService.atualizarProduto(produto), HttpStatus.OK);
+	}
+	
+	@DeleteMapping
+	public ResponseEntity<String> deletarProduto(@RequestBody Produto produto) {
+		if (Boolean.TRUE.equals(produtoService.deletarProduto(produto)))
+			return new ResponseEntity<>("Deletado com sucesso", HttpStatus.OK);
+		else
+			return new ResponseEntity<>("Não foi possível deletar", HttpStatus.BAD_REQUEST);
+	}
 }
