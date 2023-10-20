e60ceabbd40028cb8d3f5971cda6a32cdba64ed1
sabrinapereiry <sabrinapereira2125@gmail.com>
2023-10-19 23:24:47 -0300

    atalizando img controller

--- ecommerce/src/main/java/com/api/ecommerce/controllers/ProdutoController.java
+++ ecommerce/src/main/java/com/api/ecommerce/controllers/ProdutoController.java
@@ -3,7 +3,9 @@ package com.api.ecommerce.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.api.ecommerce.entities.Produto;
 import com.api.ecommerce.services.ProdutoService;
@@ -57,4 +61,42 @@ public class ProdutoController {
 		else
 			return new ResponseEntity<>("Não foi possível deletar", HttpStatus.BAD_REQUEST);
 	}
+	
+	
+	
+	// novo
+	 @PostMapping("/upload-imagem/{idProduto}")
+	    public ResponseEntity<String> uploadImagem(@PathVariable Long idProduto, @RequestParam("file") MultipartFile file) {
+	        Produto produto = produtoService.buscarProdutoPorId(idProduto);
+	        if (produto == null) {
+	            return new ResponseEntity<>("Produto não encontrado", HttpStatus.NOT_FOUND);
+	        }
+
+	        try {
+	            byte[] imagemBytes = file.getBytes();
+	            produto.setImagem(imagemBytes);
+	            produtoService.atualizarProduto(produto);
+	            return new ResponseEntity<>("Imagem carregada com sucesso", HttpStatus.OK);
+	        } catch (Exception e) {
+	            return new ResponseEntity<>("Erro ao carregar a imagem", HttpStatus.INTERNAL_SERVER_ERROR);
+	        }
+	    }
+	
+	@GetMapping("/download-imagem/{idProduto}")
+    public ResponseEntity<byte[]> downloadImagem(@PathVariable Long idProduto) {
+        Produto produto = produtoService.buscarProdutoPorId(idProduto);
+        if (produto == null || produto.getImagem() == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.IMAGE_JPEG);
+        headers.setContentLength(produto.getImagem().length);
+
+        return new ResponseEntity<>(produto.getImagem(), headers, HttpStatus.OK);
+    }
+	
+	
+	
+	
+	
 }
